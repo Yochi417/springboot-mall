c56714c80ac665f9776bfea6ba0c106930bd8ef7
yochi <zzz26476209@gmail.com>
2024-08-27 09:58:26 +0800

    「商品功能 - 實作查詢商品列表(分頁)」

--- src/main/java/com/yochi/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/yochi/springbootmall/dao/impl/ProductDaoImpl.java
@@ -31,6 +31,7 @@ public class ProductDaoImpl implements ProductDao {
 
         Map<String, Object> map = new HashMap<>();
 
+        //查詢條件
         if(productQueryParams.getCategory() != null){
             sql += " AND category = :category";
             map.put("category", productQueryParams.getCategory().name());
@@ -40,8 +41,12 @@ public class ProductDaoImpl implements ProductDao {
             map.put("search", "%" + productQueryParams.getSearch() + "%");
         }
 
+        //排序
         sql += " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
-
+        //分頁
+        sql +=" LIMIT :limit OFFSET :offset";
+        map.put("limit", productQueryParams.getLimit());
+        map.put("offset", productQueryParams.getOffset());
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
 
         return productList;
