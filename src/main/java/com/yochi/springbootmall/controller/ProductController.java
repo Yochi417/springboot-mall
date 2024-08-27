d2f4f5bcdc00a58650ccb67beb830790bba0e0ad
yochi <zzz26476209@gmail.com>
2024-08-27 11:16:57 +0800

    「商品功能 - 實作查詢商品列表(分頁 + 總比數)」

--- src/main/java/com/yochi/springbootmall/controller/ProductController.java
+++ src/main/java/com/yochi/springbootmall/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.yochi.springbootmall.dto.ProductQueryParams;
 import com.yochi.springbootmall.dto.ProductRequest;
 import com.yochi.springbootmall.model.Product;
 import com.yochi.springbootmall.service.ProductService;
+import com.yochi.springbootmall.util.Page;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -24,7 +25,7 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts(
+    public ResponseEntity<Page<Product>> getProducts(
 
             //查詢條件filtering
             @RequestParam(required = false) ProductCategory category,
@@ -47,8 +48,19 @@ public class ProductController {
         productQueryParams.setLimit(limit);
         productQueryParams.setOffset(offset);
 
+        //取得product list
         List<Product> productList = productService.getProducts(productQueryParams);
-        return ResponseEntity.status(HttpStatus.OK).body(productList);
+
+        //取得product 總數
+        Integer total = productService.countProduct(productQueryParams);
+
+        //分頁
+        Page<Product> page = new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(total);
+        page.setResults(productList);
+        return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
 
