fef4caae42ab3b01af2bf975e9fe8f76d8efe803
yochi <zzz26476209@gmail.com>
2024-09-06 19:44:13 +0800

    「訂單功能 - 創建訂單(下)」

--- src/main/java/com/yochi/springbootmall/service/OrderService.java
+++ src/main/java/com/yochi/springbootmall/service/OrderService.java
@@ -1,8 +1,11 @@
 package com.yochi.springbootmall.service;
 
 import com.yochi.springbootmall.dto.CreateOrderRequest;
+import com.yochi.springbootmall.model.Order;
 
 public interface OrderService {
+
+    Order getOrderById(Integer orderId);
 
     Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest);
 }
--- src/main/java/com/yochi/springbootmall/service/impl/OrderServiceImpl.java
+++ src/main/java/com/yochi/springbootmall/service/impl/OrderServiceImpl.java
@@ -4,6 +4,7 @@ import com.yochi.springbootmall.dao.OrderDao;
 import com.yochi.springbootmall.dao.ProductDao;
 import com.yochi.springbootmall.dto.BuyItem;
 import com.yochi.springbootmall.dto.CreateOrderRequest;
+import com.yochi.springbootmall.model.Order;
 import com.yochi.springbootmall.model.OrderItem;
 import com.yochi.springbootmall.model.Product;
 import com.yochi.springbootmall.service.OrderService;
@@ -22,6 +23,16 @@ public class OrderServiceImpl implements OrderService {
 
     @Autowired
     private ProductDao productDao;
+
+    @Override
+    public Order getOrderById(Integer orderId) {
+        Order order = orderDao.getOrderById(orderId);
+
+        List<OrderItem> orderItemList = orderDao.getOrderItemsByOrderId(orderId);
+
+        order.setOrderItemList(orderItemList);
+        return order;
+    }
 
     @Transactional
     @Override
